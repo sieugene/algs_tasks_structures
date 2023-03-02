0165ca0b00b5a2cf7e1c72fd11a83e5d688e5562
AdiyDev <81408830+AdiyDev@users.noreply.github.com>
2023-03-02 17:27:34 +0300

    Update 10_cash.ts

--- algs_and_structures/10_cash.ts
+++ algs_and_structures/10_cash.ts
@@ -1,20 +1,23 @@
 function cashFunction(fn) {
     const cash = {}
+    
     return function (n) {
         if (cash[n]) {
             console.log('Взято из кеша', cash[n])
             return cash[n]
         }
+        
         let result = fn(n)
         console.log('Посчитала функция = ', result)
         cash[n] = result
+        
         return result;
     };
 }
 
 function factorial(n) {
     let result = 1
-    while (n != 1) {
+    while (n !== 1) {
         result *= n
         n -= 1
     }
