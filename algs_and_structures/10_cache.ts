a15ebaaa215810fc6924b28fe01b72d5918fc3a6
AdiyDev <adiy.work.mail@ya.ru>
2023-04-13 11:24:26 +0300

    feat: update name cache

---
+++ algs_and_structures/10_cache.ts
@@ -0,0 +1,34 @@
+function cacheFunction(fn) {
+    const cache = {}
+    
+    return function (n) {
+        if (cache[n]) {
+            console.log('Взято из кеша', cache[n])
+            return cache[n]
+        }
+        
+        let result = fn(n)
+        console.log('Посчитала функция = ', result)
+        cache[n] = result
+        
+        return result;
+    };
+}
+
+function factorial(n) {
+    let result = 1
+    while (n !== 1) {
+        result *= n
+        n -= 1
+    }
+    return result
+}
+
+const cacheFactorial = cacheFunction(factorial)
+
+cacheFactorial(5)
+cacheFactorial(4)
+cacheFactorial(3)
+cacheFactorial(4)
+cacheFactorial(5)
+cacheFactorial(1)
