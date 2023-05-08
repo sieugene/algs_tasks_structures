517afff64fbbc27f2c84bc7f450ceaac747a2c91
AdiyDev <81408830+AdiyDev@users.noreply.github.com>
2023-05-09 00:01:10 +0300

    Update fibonachi.ts
    add max performanse solution

--- tasks/fibonachi.ts
+++ tasks/fibonachi.ts
@@ -10,22 +10,19 @@ function fibonachi(n) {
   return fibonachi(n - 1) + fibonachi(n - 2)
 }
 
-function iterationFibonachi(n) {
-  if (n <= 0) {
-    return 0
+function fibonacci(n) {
+  let a = 1
+  let b = 1
+
+  for (let i = 3; i <= n; i++) {
+    let c = a + b
+    a = b
+    b = c
   }
-  if (n <= 2) {
-    return 1
-  }
-  let prev = 1
-  let result = 1
-  for (let i = 0; i < n - 2; i++) {
-    let tmp = result
-    result += prev
-    prev = tmp
-  }
-  return result
+                        
+  return b
 }
+
 
 // console.log(fibonachi(7))
 console.log(iterationFibonachi(9))
