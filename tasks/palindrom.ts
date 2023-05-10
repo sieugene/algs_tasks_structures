41a22dfb06ac5b54e8cee18a5a69859a70ac2d83
AdiyDev <81408830+AdiyDev@users.noreply.github.com>
2023-05-10 14:02:17 +0300

    Update palindrom.ts
    add solution with arrows

--- tasks/palindrom.ts
+++ tasks/palindrom.ts
@@ -12,9 +12,23 @@
 //     return true;
 // }
 
-function palindrom(word) {
+<!-- function palindrom(word) {
   word = word.toLowerCase()
   return word === word.split("").reverse().join("")
-}
+} -->
 
-console.log(palindrom("aaaZzaAa"))
+const isPalindrome = (str) => {
+	   let left = 0
+     let right = str.length - 1
+
+     while (left < right) {
+        if (str[left] !== str[right]) {
+              return false
+				}
+        left++
+				right--
+		 }
+     return true
+}
+                                      
+console.log(isPalindrome("aaaZzaAa"))
