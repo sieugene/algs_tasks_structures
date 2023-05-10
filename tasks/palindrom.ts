cfade15d96526c00d11d7372a17cd4cf29804cb4
AdiyDev <81408830+AdiyDev@users.noreply.github.com>
2023-05-10 14:17:53 +0300

    Update palindrom.ts

--- tasks/palindrom.ts
+++ tasks/palindrom.ts
@@ -18,16 +18,16 @@
 } -->
 
 const isPalindrome = (str) => {
-	   let left = 0
+     let left = 0
      let right = str.length - 1
 
      while (left < right) {
         if (str[left] !== str[right]) {
               return false
-				}
+	}
         left++
-				right--
-		 }
+	right--
+     }
      return true
 }
                                       
