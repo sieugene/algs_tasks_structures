120493b32152511a97a648a9289b7b64c53ec0ca
AdiyDev <81408830+AdiyDev@users.noreply.github.com>
2023-05-11 19:33:37 +0300

    Update 11_linked_list.ts
    add find, afterIstert, append, prepend, remove
    add tail for linked list

--- algs_and_structures/11_linked_list.ts
+++ algs_and_structures/11_linked_list.ts
@@ -1,52 +1,119 @@
+class Node {
+    constructor(value, next = null) {
+        this.value = value
+        this.next = next
+    }
+}
+
 class LinkedList {
     constructor() {
-        this.size = 0
-        this.root = null
+       this.head = null
+       this.tail = null
+       
+       this.size = 0
     }
-
-    add(value) {
+     
+    append(value) {
         if (this.size === 0) {
-            this.root = new Node(value);
-            this.size += 1;
-            return true;
+            this.head = new Node(value)
+            
+            this.size += 1
+            return true
         }
-        let node = this.root
+        
+        let node = this.head
+       
         while (node.next) {
             node = node.next
         }
+        
         let newNode = new Node(value)
         node.next = newNode
+        
         this.size += 1
     }
-
+    
+    prepend(value) {
+        let node = new Node(value, this.head)
+       
+        this.head = node
+        
+       if (!this.head) {
+           this.tail = node
+       }
+        
+        this.size += 1
+    }
+    
     getSize() {
         return this.size
     }
 
-    print() {
-        let result = []
-        let node = this.root
-        while (node) {
-            result.push(node.value)
-            node = node.next
+    
+    find(value) {
+        if (!this.head) {
+            return
         }
-        console.log(result);
-    }
-}
 
-class Node {
-    constructor(value) {
-        this.value = value
-        this.next = null
+    let current = this.head
+    while (current) {
+      if (current.value === value) {
+        return current
+      }
+      current = current.next
     }
-}
+  }
+  
+insertAfter(after, value) {
+      const found = this.find(after)
 
+    if (!found) {
+      return
+    }
+
+    found.next = new Node(value, found.next)
+  }
+
+  print() {
+    const output = []
+    let current = this.head
+
+    while (current) {
+      output.push(current.value)
+      current = current.next
+    }
+
+    return output
+  }
+
+
+   remove(value) {
+    if (!this.head) {
+      return
+    }
+
+    while (this.head && this.head.value === value) {
+      this.head = this.head.next
+    }
+
+    let current = this.head
+    
+    while (current.next) {
+      if (current.next.value === value) {
+        current.next = current.next.next
+      } else {
+        current = current.next
+      }
+    }
+
+    if (this.tail === value) {
+      this.tail = current
+    }
+    this.size -= 1
+  }
+}
 const list = new LinkedList()
 list.add(5)
 list.add(3)
-list.add(2)
-list.add(5)
-list.add(7)
 
 list.print()
-
